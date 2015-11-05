6b5159d03f520d0edd7a8ec9d5943718f1b21b50
Vinícius Gusmão <vigusmao@gmail.com>
2015-11-05 12:36:02 -0200

    subsets - armazenando em memoria

--- subsets.py
+++ subsets.py
@@ -1,7 +1,7 @@
-def backtrack(n, min_size, max_size, subset):
+def backtrack(n, min_size, max_size, subset, all_subsets):
     # check the current state
     if min_size <= len(subset) <= max_size:
-        print(subset)
+        all_subsets.append(subset[:])
     if len(subset) == max_size:
         return
 
@@ -11,14 +11,19 @@ def backtrack(n, min_size, max_size, subset):
         biggest_element = subset[-1]
     for next_element in range(biggest_element + 1, n + 1):
         subset += [next_element]  # subset.append(next_element)
-        backtrack(n, min_size, max_size, subset)
+        backtrack(n, min_size, max_size, subset, all_subsets)
         subset.pop()
    
 
 
 def generate_subsets(n, min_size, max_size):
+    all_subsets = []
     subset = []
-    backtrack(n, min_size, max_size, subset)
+    backtrack(n, min_size, max_size, subset, all_subsets)
+    for s in all_subsets:
+        print(s)
+
+        
 
 
 
