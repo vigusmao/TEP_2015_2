e83fe9cdeb6faec83fc28c1d80d6e942a83c2f73
Vinícius Gusmão <vigusmao@gmail.com>
2015-11-19 09:57:18 -0200

    Balsas

--- ferry.py
+++ ferry.py
@@ -1,6 +1,14 @@
+from random import randint
+
+
+memo = set()
+
 def backtrack(state, vehicles, L, n_boarded_vehicles, max_boarded_vehicles):
     if state[0] > L:
         return  # invalid state
+
+    if tuple(state) in memo:
+        return  # already visited
 
     if n_boarded_vehicles > max_boarded_vehicles[0]:
         max_boarded_vehicles[0] = n_boarded_vehicles
@@ -14,6 +22,7 @@ def backtrack(state, vehicles, L, n_boarded_vehicles, max_boarded_vehicles):
     new_state.sort(reverse=True)  # [more_loaded, less_loaded] by convention
     backtrack(new_state, vehicles, L, n_boarded_vehicles + 1, max_boarded_vehicles)
 
+ #   memo.add(tuple(state))
 
 def ferry(vehicles, L):
     max_boarded_vehicles = [0]
@@ -21,3 +30,18 @@ def ferry(vehicles, L):
     backtrack(state, vehicles, L, 0, max_boarded_vehicles)
     
     print("We can board up to %d vehicles" % max_boarded_vehicles[0])
+
+
+
+queue = []
+for i in range(2000):
+    queue += [randint(100, 500)]
+
+ferry(queue, 8000)
+
+
+
+
+
+
+
